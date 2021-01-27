3a5622cf99090e2c25f56ac626187584d499a9eb
Ashrender_Kumar_Gupta <44531366+sahu121singh@users.noreply.github.com>
2021-01-28 00:47:10 +0530

    Create 12. IP Add.cpp

---
+++ 12. IP Add.cpp
@@ -0,0 +1,15 @@
+/*
+Write a program to get the IP address of the system.
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	
+	
+	system("C:\\Windows\\System32\\ipconfig");
+	
+	return 0;
+}
