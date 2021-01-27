c48c6d842c0361562e2d6d5b8fef80dcb70d9d5d
Ashrender_Kumar_Gupta <44531366+sahu121singh@users.noreply.github.com>
2021-01-28 00:49:10 +0530

    Create 13. Shutdown or Restart.cpp

---
+++ 13. Shutdown or Restart.cpp
@@ -0,0 +1,26 @@
+/*
+Write a program to shut down the system on windows operating system.
+*/
+
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+using namespace std;
+
+void main() {
+    int choice;
+    cout<<"1. Shutdown Your Computer \n";
+    cout<<"2. Restart Your Computer \n";
+    cout<<"\n Enter your choice : ";
+    cin>>choice;
+    switch(choice) {
+        case 1 : cout<<"Your system will be shutdown after 20 seconds \n";
+                 system("c:\\windows\\system32\\shutdown /s /t 20 \n\n");
+                 break;
+        case 2 : cout<<"Your system will be restarted in 20 seconds\n";
+                 system("c:\\windows\\system32\\shutdown /r /t 20\n\n");
+                 break;
+       default : cout<<"Wrong Choice..!!\n";
+   }
+   return 0;
+}
