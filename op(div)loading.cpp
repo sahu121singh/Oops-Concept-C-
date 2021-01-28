7941e2d651b235b2dd6f05ab69ec30fcbe206e76
Ashrender_Kumar_Gupta <44531366+sahu121singh@users.noreply.github.com>
2021-01-28 13:42:37 +0530

    Add files via upload

---
+++ op(div)loading.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+class complex
+{
+	float x, y;
+	public:
+		complex(){
+		}
+		complex(float a,float b)
+		{
+			x=a;
+			y=b;
+		}
+		complex operator / (complex c)
+		{
+			complex c3;
+		//	complex c3,c4;
+		//	int z;
+			
+		//	z= x*x + y*y;
+			
+			c3.x= ( x*(c.x) - y*(c.y) )/(x*x + y*y) ;
+			c3.y= ( y*(c.x) + x*(c.y) )/(x*x + y*y) ;
+		//	c4.x= (c3.x)/z;
+		//	c4.y= (c4.y)/z;
+			return (c3);
+		}
+		void display()
+		{
+			cout<<x<<" + i("<<y<<")"<<endl;
+		}
+};
+int main()
+{
+	complex c1(2,3),c2(4,5);
+	complex c3;
+	c3=c2/c1;
+	c3.display();
+	return 0;
+}
---
+++ op(mul)loading.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class complex
+{
+	int x, y;
+	public:
+		complex(){
+		}
+		complex(int a,int b)
+		{
+			x=a;
+			y=b;
+		}
+		complex operator * (complex c)
+		{
+			complex c3;
+			
+			c3.x= x*(c.x) - y*(c.y);
+			c3.y= y*(c.x) + x*(c.y);
+			
+			return c3;
+		}
+		void display()
+		{
+			cout<<x<<" + i("<<y<<")"<<endl;
+		}
+};
+int main()
+{
+	complex c1(2,3),c2(4,5);
+	c1.display();
+	c2.display();
+	complex c3;
+	c3=c1*c2;
+	c3.display();
+	return 0;
+}
