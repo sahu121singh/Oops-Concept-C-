7654e6691c49a4fa6e5a7bf7b9f4d6acd9fff27a
Ashrender_Kumar_Gupta <44531366+sahu121singh@users.noreply.github.com>
2021-01-28 13:24:01 +0530

    Add files via upload

---
+++ UNop(==)loading.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Oper
+{
+	int a,b;
+	public:
+		Oper()
+		{
+			
+		}
+		Oper(int x)
+		{
+			a=x;
+		}
+		void operator == ( Oper B);
+		void disp();
+};
+void Oper :: operator == ( Oper B)
+{
+	if (a==B.a)
+	cout<<"equal";
+	else
+	cout<<"uequal";
+}
+void Oper :: disp()
+{
+	cout<<a<<endl;
+}
+int main()
+{
+	Oper p(10),q(5);
+	p.disp();
+	q==p;
+	return 0;
+}
