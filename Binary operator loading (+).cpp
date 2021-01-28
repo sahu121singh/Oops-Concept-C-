19b2ad2febf52012d6facde8bae31c02fc0ed3ea
Ashrender_Kumar_Gupta <44531366+sahu121singh@users.noreply.github.com>
2021-01-28 15:17:42 +0530

    Update and rename op(+)loading.cpp to Binary operator loading (+).cpp

--- Binary operator loading (+).cpp
+++ Binary operator loading (+).cpp
@@ -1,36 +1,32 @@
 #include<iostream>
 using namespace std;
 
-class complex
-{
+class complex {
 	int x, y;
 	public:
 		complex(){}
 		
-		complex(int a,int b)
-		{
+		complex(int a,int b) {
 			x=a;
 			y=b;
 		}
-		complex operator + (complex C)
-		{
+		complex operator + (complex C) {
 			complex t;
 			t.x = x + C.x;
 			t.y = y + C.y;
 			return t;
 		}
-		void display()
-		{
+		void display() {
 			cout<<x<<" + i"<<y<<endl;
 		}
 };
-int main()
-{
+
+int main() {
 	complex C1(2,3),C2(4,5);
 	complex C3;
 	C1.display();
 	C2.display();
 	C3=C1+C2;
-    C3.display();
+	C3.display();
 	return 0;
 }
